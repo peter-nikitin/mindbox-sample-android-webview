ad0779790c0b7c714efcc9d1feeddc00bb920705
Petr <nikitin.p.94@gmail.com>
2021-09-16 16:14:37 +0300

    add webview

--- app/src/main/java/com/akkalomator/mindboxjssdktest/MainActivity.kt
+++ app/src/main/java/com/akkalomator/mindboxjssdktest/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import android.webkit.CookieManager
+import android.webkit.WebView
 import cloud.mindbox.mobile_sdk.Mindbox
 import com.akkalomator.mindboxjssdktest.databinding.ActivityMainBinding
 
